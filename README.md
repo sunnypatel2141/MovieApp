854df95116cda1003cf5ed8cb4ece9016e60d7ff
sunnypatel2141 <sanipatel2141@hotmail.com>
2017-11-25 17:53:08 -0500

    Finished implementation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Movie App
-This app used OMBDb API to retrive user-specified information such as related movies, and detailed information about selected movies.
+This application allows user to enter a movie title, and retrieve movie listing that matches the search. The application also allows uses to click on a specific movie and access information such as actors, directors of the movie. There's an option to view the IMDb page for the specific movie with a simple click of a button.  
+
+Note: Axios client used to make RESTful calls  
+Note: SessionStorage used to retrieve movie ID
--- js/main.js
+++ js/main.js
@@ -31,3 +31,66 @@ function getMovieDetails(searchText)
       console.log(error);
     });
 }
+
+function movieSelected(id)
+{
+    sessionStorage.setItem('movieId', id);
+    window.location = 'movie.html';
+    return false;
+}
+
+function getMovie()
+{
+    let movieId = sessionStorage.getItem('movieId');
+    var api = "&apikey=c4dacaef"
+    var url = "http://www.omdbapi.com/?i=" + movieId + api;
+    axios.get(url)
+    .then(function (response) {
+        var movie = response.data;
+        var output = `
+                    <div class="row">
+                        <div class="col-md-4">
+                        <img src="${movie.Poster}" class="thumbnail">
+                        </div>
+                        <div class="col-md-8">
+                            <h2>${movie.Title}</h2>
+                            <ul class="list-group">
+                                <li class="list-group-item">
+                                    Genre: ${movie.Genre}
+                                </li>
+                                <li class="list-group-item">
+                                    Release: ${movie.Released}
+                                </li>
+                                <li class="list-group-item">
+                                    Rated: ${movie.Rated}
+                                </li>
+                                <li class="list-group-item">
+                                    IMDb Rating: ${movie.imdbRating}
+                                </li>
+                                <li class="list-group-item">
+                                    Director(s): ${movie.Director}
+                                </li>
+                                <li class="list-group-item">
+                                    Writer(s): ${movie.Writer}
+                                </li>
+                                <li class="list-group-item">
+                                    Actors: ${movie.Actors}
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="row>
+                        <div class="well">
+                            <h3> Plot: </h3> ${movie.Plot}
+                            </br>
+                            <a href = "http://imdb.com/title/${movie.imdbID}" target="blank" class="btn btn-primary">View IMDb</a>
+                            <a href="index.html" class="btn btn-default">Back to Search</a>
+                        </div>
+                    </div>
+        `
+        $('#movie').html(output);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
