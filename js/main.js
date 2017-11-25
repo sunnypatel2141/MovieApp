c40d6db1d0e4c8ac76649beefcb9b44f86ee3b1e
sunnypatel2141 <sanipatel2141@hotmail.com>
2017-11-25 17:26:03 -0500

    Retrieve movie info

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
 $(document).ready(() => {
     $('#searchForm').on('submit', (e) => {
-        console.log($('#searchText').val());
+        var searchText = $('#searchText').val();
+        getMovieDetails(searchText);
+        
         e.preventDefault();
     });
 });
+
+function getMovieDetails(searchText)
+{
+    var api = "&apikey=c4dacaef"
+    var url = "http://www.omdbapi.com/?s=" + searchText + api;
+    axios.get(url)
+    .then(function (response) {
+        var movielist = response.data.Search;
+        var output = "";
+        $.each(movielist, (index, movie) => {
+            output += `<div class="col-md-3">
+                        <div class="well text-center">
+                            <img src="${movie.Poster}">
+                            <h5>${movie.Title}</h5>
+                            <a onclick="movieSelected('${movie.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+                        </div>
+                    </div>`
+        });
+        $('#movies').html(output);
+        console.log(output);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
